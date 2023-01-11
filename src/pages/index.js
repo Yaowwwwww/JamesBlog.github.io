7190162c201dd9596fe7f2ba7f88c862ec12b37a
soto <59144459+acsoto@users.noreply.github.com>
2023-01-11 21:16:54 +0800

    add card

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,65 +4,76 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import {StaticImage} from "gatsby-plugin-image";
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
 
           return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
+                  <li key={post.fields.slug}>
+                      <Card {...post} />
+                      {/*<article*/}
+                      {/*    className="post-list-item"*/}
+                      {/*    itemScope*/}
+                      {/*    itemType="http://schema.org/Article"*/}
+                      {/*>*/}
+                      {/*    <header>*/}
+                      {/*        <h2>*/}
+                      {/*            <Link to={post.fields.slug} itemProp="url">*/}
+                      {/*                <span itemProp="headline">{title}</span>*/}
+                      {/*            </Link>*/}
+                      {/*        </h2>*/}
+                      {/*        <small>{post.frontmatter.date}</small>*/}
+                      {/*    </header>*/}
+                      {/*    <section>*/}
+                      {/*        <p*/}
+                      {/*            dangerouslySetInnerHTML={{*/}
+                      {/*                __html: post.frontmatter.description || post.excerpt,*/}
+                      {/*            }}*/}
+                      {/*            itemProp="description"*/}
+                      {/*        />*/}
+                      {/*    </section>*/}
+                      {/*</article>*/}
+                  </li>
           )
         })}
       </ol>
     </Layout>
   )
 }
+
+const Card = (post) => (
+    <Link to={post.fields.slug} itemProp="url">
+        <div className={card}>
+            <article className="post-list-item" itemScope itemType="http://schema.org/Article">
+                <header>
+                    <h1 className="text-3xl font-bold">
+                        <span itemProp="headline">{post.frontmatter.title}</span>
+                    </h1>
+                    <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                    <p
+                        dangerouslySetInnerHTML={{
+                            __html: post.frontmatter.description || post.excerpt,
+                        }}
+                        itemProp="description"
+                    />
+                </section>
+            </article>
+        </div>
+    </Link>
+)
+
+const card = "p-6 max-w-64 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
 
 export default BlogIndex
 
