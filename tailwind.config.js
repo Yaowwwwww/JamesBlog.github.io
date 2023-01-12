407c1641b31088395455a7ff452a32c2999eae02
soto <59144459+acsoto@users.noreply.github.com>
2023-01-13 01:07:20 +0800

    add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ module.exports = {
     "./src/templates/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      'green-soto':'#ABB581'
+    },
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require("daisyui"),
   ],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
