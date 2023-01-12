d30ad204d3ccd4134832de19499a29146e2d58e1
soto <59144459+acsoto@users.noreply.github.com>
2023-01-12 22:11:02 +0800

    排版

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,69 +1,40 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
+import * as React from "react";
+import graphql from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const BlogPostTemplate = ({
-  data: { previous, next, site, markdownRemark: post },
-  location,
-}) => {
-  const siteTitle = site.siteMetadata?.title || `Title`
+                            data: { site, markdownRemark: post },
+                            location
+                          }) => {
+  const siteTitle = site.siteMetadata?.title || `Title`;
 
   return (
     <Layout location={location} title={siteTitle}>
       <article
-        className="
-        prose md:prose-lg lg:prose-xl
-        prose-img:rounded-xl
-        prose-a:text-blue-500 hover:prose-a:text-blue-400
-        "
         itemScope
         itemType="https://schema.org/Article"
       >
         <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
+          <h1 className={"text-4xl font-bold my-3"} itemProp="headline">{post.frontmatter.title}</h1>
+          <p className={"text-md text-gray-500 my-3"}>{post.frontmatter.date}</p>
+          <img className={"rounded-xl my-3"} src={post.frontmatter.image} alt={"cover"} />
         </header>
         <section
+          className="
+          prose md:prose-lg lg:prose-xl
+          prose-img:rounded-xl
+          prose-a:text-blue-500 hover:prose-a:text-blue-400
+          "
           dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
         />
         <hr />
-        <footer>
-          <Bio />
-        </footer>
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
     </Layout>
-  )
-}
+  );
+};
 
 export const Head = ({ data: { markdownRemark: post } }) => {
   return (
@@ -71,10 +42,10 @@ export const Head = ({ data: { markdownRemark: post } }) => {
       title={post.frontmatter.title}
       description={post.frontmatter.description || post.excerpt}
     />
-  )
-}
+  );
+};
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug(
@@ -95,6 +66,7 @@ export const pageQuery = graphql`
         title
         date(formatString: "YYYY-MM-DD")
         description
+        image
       }
     }
     previous: markdownRemark(id: { eq: $previousPostId }) {
@@ -114,4 +86,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
