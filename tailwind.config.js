1dcee528dabb90269164173801a292020b0cf42c
soto <59144459+acsoto@users.noreply.github.com>
2023-01-13 10:44:32 +0800

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,53 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/templates/**/*.{js,jsx,ts,tsx}",
+    "./src/templates/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    colors:{
-      'green-soto':'#ABB581'
+    colors: {
+      "green-soto": "#ABB581"
     },
-    extend: {},
+    extend: {}
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui"),
+    require("@tailwindcss/typography"),
+    require("daisyui")
   ],
   daisyui: {
     styled: true,
-    themes: false,
+    themes: [
+      {
+        soto_theme: {
+
+          "primary": "#65a30d",
+
+          "secondary": "#84cc16",
+
+          "accent": "#a3e635",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+      "dark",
+      "cupcake"
+    ],
     base: false,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "dark",
-  },
-}
+    darkTheme: "dark"
+  }
+};
